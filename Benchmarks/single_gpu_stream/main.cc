6abf4f1ec1611a441cd432e9441115d2fc47840e
Siram Katta <sriram.katta@fau.de>
2025-04-07 00:55:34 +0200

    Increase data size for benchmark and correct stream parameter in asyncMemcpyD2H call

--- Benchmarks/single_gpu_stream/main.cc
+++ Benchmarks/single_gpu_stream/main.cc
@@ -10,16 +10,17 @@ namespace CH = cuda_helpers;
 namespace SH = stream_helper;
 
 int main(int argc, char const *argv[]) {
-  size_t N = 1024 * 1024;
+  size_t N = 512 * 1024 * 1024;
   auto devptr = CH::allocDevice<double>(N);
   auto hosptr = CH::allocHost<double>(N);
   CHECK_CUDA_ERR(cudaMemset(devptr.get(), 0, N * sizeof(double)));
   SH::cudaStream stream1;
+  SH::cudaStream stream2;
   auto ptr = devptr.get();
   stream<<<50, 256, 0, stream1>>>(N, ptr);
   CHECK_CUDA_LASTERR("STREAM KERNEL LAUNCH FAILED");
   CHECK_CUDA_ERR(cudaDeviceSynchronize());
-  CH::asyncMemcpyD2H(devptr, hosptr, N, stream);
+  CH::asyncMemcpyD2H(devptr, hosptr, N, stream2);
   CHECK_CUDA_ERR(cudaDeviceSynchronize());
   return 0;
 }
