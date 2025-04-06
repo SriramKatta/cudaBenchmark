08081313ea38b18d00992177e9c5246e40793885
Siram Katta <sriram.katta@fau.de>
2025-04-07 01:54:15 +0200

    just to check

--- Benchmarks/single_gpu_stream/main.cu
+++ Benchmarks/single_gpu_stream/main.cu
@@ -18,20 +18,19 @@ void fill(VT *ptr, size_t N) {
   }
 }
 
-
 int main(int argc, char const *argv[]) {
   size_t N = 512 * 1024 * 1024;
   auto dev_data = CH::allocDevice<double>(N);
   auto hos_data = CH::allocHost<double>(N);
   fill(hos_data.get(), N);
   size_t chunk = N / numstream;
-  #pragma omp parallel for 
+#pragma omp parallel for
   for (size_t i = 0; i < numstream; i++) {
     SH::cudaStream stream;
     auto h_ptr = hos_data.get() + (i * chunk);
     auto d_ptr = dev_data.get() + (i * chunk);
     CH::asyncMemcpyH2D(h_ptr, d_ptr, chunk, stream);
-    streamkernel<<<36,256,0, stream>>>(chunk, d_ptr);
+    streamkernel<<<36, 256, 0, stream>>>(chunk, d_ptr);
     CH::asyncMemcpyD2H(d_ptr, h_ptr, chunk, stream);
   }
 
