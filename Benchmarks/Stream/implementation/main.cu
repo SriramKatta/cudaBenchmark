fb9f6590fef6914582ddacc75c46461176965231
Siram Katta <sriram.katta@fau.de>
2025-04-19 12:29:53 +0200

    Refactor Stream benchmark structure: remove header directory, update CMake configuration, and enhance command line argument parsing

---
+++ Benchmarks/Stream/implementation/main.cu
@@ -0,0 +1,63 @@
+#include <fmt/format.h>
+#include <exception>
+
+#include "cuda_errror_handler.cuh"
+#include "cuda_helper.cuh"
+#include "kernels.hpp"
+#include "parseCLA.hpp"
+#include "stream_helper.cuh"
+
+namespace CH = cuda_helpers;
+namespace SH = stream_helper;
+template <typename VT>
+void checkSolution(VT *data, size_t N, size_t reps) {
+  VT error = static_cast<VT>(0);
+  for (size_t i = 0; i < N; i++) {
+    error += std::abs(data[i] - reps);
+  }
+  fmt::print("error : {}\n", error);
+}
+
+int main(int argc, char const *argv[]) {
+  size_t N;
+  size_t NumReps;
+  size_t NumBlocks;
+  size_t NumThredsPBlock;
+  size_t NumStreams;
+  try {
+    parseCLA(argc, argv, N, NumReps, NumBlocks, NumThredsPBlock, NumStreams);
+  } catch (std::exception &e) {
+    fmt::print("Error : {}\n", e.what());
+    exit(1);
+  }
+  auto *dev = CH::allocDevice<double>(N);
+  auto *host = CH::allocHost<double>(N);
+  for (size_t i = 0; i < N; i++) {
+    host[i] = 0.0;
+  }
+
+  for (size_t rep = 0; rep < NumReps; rep++) {
+    size_t baseChunkSize = N / NumStreams;
+    size_t remainder = N % NumStreams;
+    size_t chunkstart = 0;
+    for (size_t i = 0; i < NumStreams; i++) {
+      size_t currentChunkSize = baseChunkSize + (i < remainder ? 1 : 0);
+      SH::cudaStream stream;
+
+      CH::asyncMemcpyH2D(host + chunkstart, dev + chunkstart, currentChunkSize,
+                         stream);
+      stream_kernel<<<NumBlocks, NumThredsPBlock>>>(dev + chunkstart,
+                                                    currentChunkSize);
+      CHECK_CUDA_LASTERR("Stream Launch failure");
+      CH::asyncMemcpyD2H(dev + chunkstart, host + chunkstart, currentChunkSize,
+                         stream);
+      chunkstart += currentChunkSize;
+    }
+  }
+  CHECK_CUDA_ERR(cudaDeviceSynchronize());
+  checkSolution(host, N, NumReps);
+
+  CHECK_CUDA_ERR(cudaFree(dev));
+  CHECK_CUDA_ERR(cudaFreeHost(host));
+  return 0;
+}
--- Benchmarks/Stream/utilities/parseCLA.hpp
+++ Benchmarks/Stream/utilities/parseCLA.hpp
@@ -6,40 +6,43 @@
 #include <boost/program_options.hpp>
 #include <iostream>
 namespace po = boost::program_options;
-void parseCLA(int argc, char const *argv[], size_t &N, size_t &NumReps,
-              size_t &NumBlocks, size_t &NumThredsPBlock) {
-  po::options_description desc("Allowed Options");
+template <typename IT>
+void parseCLA(int argc, char const *argv[], IT &N, IT &NumReps, IT &NumBlocks,
+              IT &NumThredsPBlock, IT& numStreams) {
+  po::options_description desc("Allowed Options", 100);
   // clang-format off
     desc.add_options()
     ("help,h", "produce help message")
-    ("num_elements,N", po::value<size_t>(&N)->default_value(100), "set number of elements")
-    ("reps,R", po::value<size_t>(&NumReps)->default_value(16), "set num of kernel repetions")
-    ("blocks,B", po::value<size_t>(&NumBlocks)->default_value(30), "set number of blocks")
-    ("threads_per_block,T", po::value<size_t>(&NumThredsPBlock)->default_value(128), "set number of threads per block")
+    ("num_elements,N", po::value<IT>(&N)->default_value(100), "set number of elements")
+    ("reps,R", po::value<IT>(&NumReps)->default_value(16), "set num of kernel repetions")
+    ("blocks,B", po::value<IT>(&NumBlocks)->default_value(30), "set number of blocks")
+    ("threads_per_block,T", po::value<IT>(&NumThredsPBlock)->default_value(128), "set number of threads per block")
+    ("Streams,S", po::value<IT>(&numStreams)->default_value(4), "set number of threads per block")
     ;
   // clang-format on
   po::variables_map vm;
   po::store(po::parse_command_line(argc, argv, desc), vm);
   po::notify(vm);
-
+  if (vm.count("help")) {
+    std::cout << desc;
+    exit(0);
+  }
   if (vm.count("num_elements")) {
-    N = vm["num_elements"].as<size_t>();
+    N = vm["num_elements"].as<IT>();
   }
   if (vm.count("reps")) {
-    NumReps = vm["reps"].as<size_t>();
+    NumReps = vm["reps"].as<IT>();
   }
   if (vm.count("blocks")) {
-    NumBlocks = vm["blocks"].as<size_t>();
+    NumBlocks = vm["blocks"].as<IT>();
   }
   if (vm.count("threads_per_block")) {
-    NumThredsPBlock = vm["threads_per_block"].as<size_t>();
+    NumThredsPBlock = vm["threads_per_block"].as<IT>();
   }
-  if (vm.count("help") || argc == 1) {
-    std::cout << desc;
-    fmt::print(
-      "N : {} | Nreps : {} | NumBlocks : {} | NumthreadsPblocks : {}\n", N,
-      NumReps, NumBlocks, NumThredsPBlock);
+  if (vm.count("Streams")) {
+    numStreams = vm["Streams"].as<IT>();
   }
+
 }
 
 #endif
