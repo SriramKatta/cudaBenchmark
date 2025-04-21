e3a32c7d18dca5382397b76a73341e21b406c897
Siram Katta <sriram.katta@fau.de>
2025-04-21 13:30:30 +0200

    Enhance performance measurement: differentiate main loop versions and report elapsed time for each

--- Benchmarks/Stream/implementation/main.cu
+++ Benchmarks/Stream/implementation/main.cu
@@ -44,7 +44,7 @@ int main(int argc, char const *argv[]) {
   TH::cudaTimer fullwork;
   fullwork.start();
   for (size_t rep = 0; rep < NumReps; rep++) {
-    nvtx3::scoped_range loop{"main loop"};
+    nvtx3::scoped_range loop{"main loop V1"};
     size_t baseChunkSize = N / NumStreams;
     size_t remainder = N % NumStreams;
     size_t chunkstart = 0;
@@ -63,9 +63,45 @@ int main(int argc, char const *argv[]) {
     }
     CHECK_CUDA_ERR(cudaDeviceSynchronize());
   }
+  fullwork.stop();
   auto elapsed_time = fullwork.elapsedSeconds() / NumReps;
-  fmt::print("elapsed time per rep is {}", elapsed_time);
-  checkSolution(host, N, NumReps);
+  fmt::print("elapsed time V1 per rep is {}\n", elapsed_time);
+  //checkSolution(host, N, NumReps);
+
+  fullwork.start();
+  for (size_t rep = 0; rep < NumReps; rep++) {
+    nvtx3::scoped_range loop{"main loop V2"};
+    size_t baseChunkSize = N / NumStreams;
+    size_t remainder = N % NumStreams;
+    //size_t chunkstart = 0;
+    std::vector<SH::cudaStream> streams(NumStreams);
+    for (size_t chunkstart = 0, i = 0; i < NumStreams; i++) {
+      size_t currentChunkSize = baseChunkSize + (i < remainder ? 1 : 0);
+      CH::asyncMemcpyH2D(host + chunkstart, dev + chunkstart, currentChunkSize,
+                         streams[i]);
+      chunkstart += currentChunkSize;
+    }
+    for (size_t chunkstart = 0, i = 0; i < NumStreams; i++) {
+      size_t currentChunkSize = baseChunkSize + (i < remainder ? 1 : 0);
+      stream_kernel<<<NumBlocks, NumThredsPBlock, 0, streams[i]>>>(
+        dev + chunkstart, currentChunkSize);
+      CHECK_CUDA_LASTERR("Stream Launch failure");
+      chunkstart += currentChunkSize;
+    }
+
+
+    for (size_t chunkstart = 0, i = 0; i < NumStreams; i++) {
+      size_t currentChunkSize = baseChunkSize + (i < remainder ? 1 : 0);
+      CH::asyncMemcpyD2H(dev + chunkstart, host + chunkstart, currentChunkSize,
+                         streams[i]);
+      chunkstart += currentChunkSize;
+    }
+    CHECK_CUDA_ERR(cudaDeviceSynchronize());
+  }
+  fullwork.stop();
+  elapsed_time = fullwork.elapsedSeconds() / NumReps;
+  fmt::print("elapsed time V2 per rep is {}\n", elapsed_time);
+
 
   return 0;
 }
