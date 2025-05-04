96da4c7dc1904eb2109a785c575687f9aa3527ed
Siram Katta <sriram.katta@fau.de>
2025-05-04 20:20:00 +0200

    Refactor main function: streamline verbose output handling and consolidate elapsed time reporting

--- Benchmarks/Stream/implementation/main.cu
+++ Benchmarks/Stream/implementation/main.cu
@@ -24,6 +24,7 @@ int main(int argc, char const *argv[]) {
 
   auto sizeinGB = CH::sizeInGBytes(dev_ptr, N);
 
+
   auto dev = dev_ptr.get();
   auto host = host_ptr.get();
 
@@ -35,17 +36,11 @@ int main(int argc, char const *argv[]) {
     NumReps, N, NumStreams, host, dev, NumBlocks, NumThredsPBlock, verbose);
   fullwork.stop();
 
-  if (verbose)
-    fmt::print("{} {:.3f} {:.3f} {:.3f}\n", static_cast<size_t>(sizeinGB * 1e9),
-               sizeinGB / h2dv1, sizeinGB * 2.0 / kernv1, sizeinGB / d2hv1);
-
-
   if (doCheck) {
     checkSolution(host, N, NumReps);
   }
 
-  auto elapsed_time = fullwork.elapsedSeconds() / NumReps;
-  fmt::print("elapsed time V1 per rep is {}\n", elapsed_time);
+  auto elapsedV1 = fullwork.elapsedSeconds() / NumReps;
 
   CH::initHost(host_ptr, N, 0.0);
 
@@ -60,13 +55,19 @@ int main(int argc, char const *argv[]) {
     checkSolution(host, N, NumReps);
   }
 
-  if (verbose)
-    fmt::print("{} {:.3f} {:.3f} {:.3f}\n", static_cast<size_t>(sizeinGB * 1e9),
-               sizeinGB / h2dv2, sizeinGB * 2.0 / kernv2, sizeinGB / d2hv2);
+  auto elapsedV2 = fullwork.elapsedSeconds() / NumReps;
 
+  if (verbose) {
+    fmt::print(
+      "D={},R={},B={},T={},S={}\n"
+      "V1={:.8f},V1_h2d={:.3f},V1_kernel={:.3f},V1_d2h={:.3f}\n"
+      "V2={:.8f},V2_h2d={:.3f},V2_kernel={:.3f},V2_d2h={:.3f}\n",
+      static_cast<size_t>(sizeinGB * 1e9), NumReps, NumBlocks, NumThredsPBlock,
+      NumStreams, elapsedV1, sizeinGB / h2dv1, sizeinGB * 2.0 / kernv1,
+      sizeinGB / d2hv1, elapsedV2, sizeinGB / h2dv2, sizeinGB * 2.0 / kernv2,
+      sizeinGB / d2hv2);
+  }
 
-  elapsed_time = fullwork.elapsedSeconds() / NumReps;
-  fmt::print("elapsed time V2 per rep is {}\n", elapsed_time);
 
   return 0;
 }
