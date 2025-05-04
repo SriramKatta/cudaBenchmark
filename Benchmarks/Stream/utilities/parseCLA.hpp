3e01fcb15dec715b3b9b5d3f95f3b87b2b61a402
Siram Katta <sriram.katta@fau.de>
2025-05-04 20:01:22 +0200

    Refactor parseCLA function: update default values for command line options and improve help message handling

--- Benchmarks/Stream/utilities/parseCLA.hpp
+++ Benchmarks/Stream/utilities/parseCLA.hpp
@@ -16,29 +16,37 @@ namespace CH = cuda_helpers;
 using IT = size_t;
 
 void parseCLA(int argc, char const *argv[], IT &N, IT &NumReps, IT &NumBlocks,
-              IT &NumThredsPBlock, IT &numStreams, bool& doCheck) {
+              IT &NumThredsPBlock, IT &numStreams, bool &verbose,
+              bool &doCheck) {
+  try {
 
-  po::options_description desc(fmt::format("Usage : {} [OPTIONS]", argv[0]),
-                               100);
-  // clang-format off
-    desc.add_options()
-    ("help,h", "produce help message")
-    ("num_elements,N", po::value<IT>(&N)->default_value(100), "set number of elements")
-    ("reps,R", po::value<IT>(&NumReps)->default_value(16), "set num of kernel repetions")
-    ("blocks,B", po::value<IT>(&NumBlocks)->default_value(CH::getSMCount() * 16), "set number of blocks")
-    ("threads_per_block,T", po::value<IT>(&NumThredsPBlock)->default_value(CH::getWarpSize() * 16), "set number of threads per block")
-    ("Streams,S", po::value<IT>(&numStreams)->default_value(4), "set number of threads per block")
-    ("CHECK,C", po::bool_switch(&doCheck)->default_value(true), "set number of threads per block")
-    ;
-  // clang-format on
-  po::variables_map vm;
-  const auto parsedoptions = po::command_line_parser(argc, argv).options(desc).run();
-  po::store(parsedoptions, vm);
-  po::notify(vm);
+    po::options_description desc(fmt::format("Usage : {} [OPTIONS]", argv[0]),
+                                 100);
+    // clang-format off
+  desc.add_options()
+  ("help,h", "produce help message")
+  ("num_elements,N", po::value<IT>(&N)->default_value(1<<25), "set number of elements")
+  ("reps,R", po::value<IT>(&NumReps)->default_value(16), "set num of kernel repetions")
+  ("blocks,B", po::value<IT>(&NumBlocks)->default_value(CH::getSMCount()), "set number of blocks")
+  ("threads_per_block,T", po::value<IT>(&NumThredsPBlock)->default_value(CH::getWarpSize()), "set number of threads per block")
+  ("Streams,S", po::value<IT>(&numStreams)->default_value(1), "set number of threads per block")
+  ("VERBOSE,V", po::bool_switch(&verbose)->default_value(false), "print full performance timings/bandwidths")
+  ("CHECK,C", po::bool_switch(&doCheck)->default_value(true), "perform check to verify kernel correctness")
+  ;
+    // clang-format on
+    po::variables_map vm;
+    const auto parsedoptions =
+      po::command_line_parser(argc, argv).options(desc).run();
+    po::store(parsedoptions, vm);
+    po::notify(vm);
 
-  if (vm.count("help")) {
-    std::cout << desc;
-    exit(0);
+    if (vm.count("help")) {
+      std::cout << desc;
+      exit(EXIT_SUCCESS);
+    }
+  } catch (std::exception &e) {
+    fmt::print("Error : {}\n", e.what());
+    exit(1);
   }
 }
 
